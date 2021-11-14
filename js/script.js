bc97fbcdb06e461c3569b53c91c335cfe2cea86a
MacBook <macbook@zef.Dlink>
2021-11-14 18:50:39 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 11;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+console.log(obj);
+console.log(name);
+
+var name = 'Ivan'; 
