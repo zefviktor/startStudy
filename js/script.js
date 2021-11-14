efa48943dce7fe3304b48e85b30431f43b5a10c5
zefviktor <zefviktor@gmail.com>
2021-11-14 21:33:21 +0200

    Add homework - 1.3

--- js/script.js
+++ js/script.js
@@ -33,3 +33,14 @@ let personalMovieDB = {
     genres : [],
     privar : false
 };
+//3
+let a = prompt('Один из последних просмотренных фильмов?','');
+let b = prompt('На сколько оцените его?','');
+let c = prompt('Один из последних просмотренных фильмов?','');
+let d = prompt('На сколько оцените его?','');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
